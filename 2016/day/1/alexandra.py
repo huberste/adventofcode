d58c72efce35d8e58c642d95ac562aa880ca3a3c
Stefan Huber <stefan.huber@stusta.de>
2016-12-04 22:35:07 +0100

    alexandra hat tag 1 fertig

--- 2016/day/1/alexandra.py
+++ 2016/day/1/alexandra.py
@@ -15,17 +15,54 @@ print(directions[5][0] + directions[5][1:])
 
 #Nord=0 Ost=1, Süd=2, West=3
 facing = 0
-distances = {"0":0, "1":0, "2":0, "3":0}
+#distances = {"0":0, "1":0, "2":0, "3":0}
+pois=[]
+
+pois.append("0,0")
+#pois.append(str(x) + "," + str(y))
+x=0
+y=0
+
+end = False
 
 for direction in directions:
+	if (end):
+		break
 	if (direction[0] == "R"):
 		facing = (facing + 1) % 4 
 	else:
 		facing = (facing -1) % 4
-	
-	distances[str(facing)]=distances[str(facing)]+int(direction[1:])
 
-print(distances)
+	if (facing % 2 ==0):	
+		#way = 1 für Norden, way = -1 für Süden
+		way = -facing + 1
+		for i in range (0,int(direction[1:])):
+			y += way
+			if ((str(x) + "," + str(y)) in pois):
+				print("Gefunden:" + (str(x) + "," + str(y)))
+				end = True
+				break
+			else:
+				pois.append(str(x) + "," + str(y))
 
-sol= abs(distances["0"]-distances["2"]) + abs(distances["1"] - distances["3"])
-print("Solution:" + str(sol))
+	else:
+		#way = 1 für Ost, way = -1 für West
+		way = -facing +2
+		for i in range (0,int(direction[1:])):
+			x += way
+			if ((str(x) + "," + str(y)) in pois):
+				print("Gefunden:" + (str(x) + "," + str(y)))
+				end = True
+				break
+			else:
+				pois.append(str(x) + "," + str(y))
+		
+print("Solution part 2:" + str(x+y)) 
+
+#	distances[str(facing)]=distances[str(facing)]+int(direction[1:])		
+#print(distances)
+
+#sol= abs(distances["0"]-distances["2"]) + abs(distances["1"] - distances["3"])
+#print("Solution:" + str(sol))
+
+
