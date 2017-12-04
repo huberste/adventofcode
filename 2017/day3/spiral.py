a683978c6a79290ca1fa48adee3135b9557ed8f2
Stefan Huber <stefan@BADWLRZ-CWHUBER.lrz.de>
2017-12-04 09:52:01 +0100

    finished solution for 2017/day3. Solution messy. :-D

--- 2017/day3/spiral.py
+++ 2017/day3/spiral.py
@@ -8,96 +8,89 @@
 # 20  7  8  9 10
 # 21 22 23 24 25 ...
 
-input = 325489
-
-# part 1
-# output: Manhattan-Distance from input to 1
-
 # nth_odd_numer = 2n-1
 # square_of_nth_odd_number = (2n-1)*(2n-1)
 # square_of_nth_odd_number is always bottom right of the spiral memory.
 
-# first we look which odd square number is the next eqal-or-larger:
+day_input = 325489
 
-ring = 0
-square = 0
-while (square < input): 
-	ring = ring + 1
-	square = (2*ring-1)*(2*ring-1)
-	
-print("the number is in the %dth ring" % ring)
+# part 1
+# output: Manhattan-Distance from input to 1
+def part1(input):
+# first we look which odd square number is the next eqal-or-larger:
+    ring = 0
+    square = 0
+    while (square < input): 
+            ring = ring + 1
+            square = (2*ring-1)*(2*ring-1)
+            
 
 # the nth ring has (2n-1) spaces in each direction.
 # we now check on which quarter of the ring we are:
 
 # the calculated square number is on the bottom right of the ring.
-bottom_right = square
-print("bottom_right: %d" % bottom_right)
-if input == bottom_right:
-	print("input is bottom_right.")
-	print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+    bottom_right = square
+    if input == bottom_right:
+            print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+            return
 
 # if we subtract (2n-1) - 1 from bottom_right, we go to bottom_left:
-bottom_left = bottom_right - ((2*ring) - 2)
-print("bottom_left: %d" % bottom_left)
-if input == bottom_left:
-	print("input is bottom_left.")
-	print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+    bottom_left = bottom_right - ((2*ring) - 2)
+    if input == bottom_left:
+            print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+            return
 
 # let's check, if the input is on the bottom row:
-if input > bottom_left:
-	print("input is in bottom row.")
-	mid = bottom_left + ring - 1
-	distance_to_mid = input - mid
-	if distance_to_mid < 0:
-		distance_to_mid = -distance_to_mid
-	distance = (ring-1) + distance_to_mid
-	print("manhattan-distance to 1: %d" % distance)
-
+    if input > bottom_left:
+            mid = bottom_left + ring - 1
+            distance_to_mid = input - mid
+            if distance_to_mid < 0:
+                    distance_to_mid = -distance_to_mid
+            distance = (ring-1) + distance_to_mid
+            print("manhattan-distance to 1: %d" % distance)
+            return
 
 # if we subtract (2n-1) - 1 from bottom_left, we go to top_left:
-top_left = bottom_left - ((2*ring) - 2)
-print("top_left: %d" % top_left)
-if input == top_left:
-	print("input is top_left.")
-	print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+    top_left = bottom_left - ((2*ring) - 2)
+    if input == top_left:
+            print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+            return
 
 # let's check, if the input is on the left column:
-if input > top_left:
-	print("input is in left column.")
-	mid = top_left + ring - 1
-	distance_to_mid = input - mid
-	if distance_to_mid < 0:
-		distance_to_mid = -distance_to_mid
-	distance = (ring-1) + distance_to_mid
-	print("manhattan-distance to 1: %d" % distance)
+    if input > top_left:
+            mid = top_left + ring - 1
+            distance_to_mid = input - mid
+            if distance_to_mid < 0:
+                    distance_to_mid = -distance_to_mid
+            distance = (ring-1) + distance_to_mid
+            print("manhattan-distance to 1: %d" % distance)
+            return
 
 # if we subtract (2n-1) - 1 from top_left, we go to top_right:
-top_right = top_left - ((2*ring) - 2)
-print("top_right: %d" % top_right)
-if input == top_right:
-	print("input is top_right.")
-	print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+    top_right = top_left - ((2*ring) - 2)
+    if input == top_right:
+            print("manhattan-distance to 1: %d" % (ring-1 +  ring-1))
+            return
 
 # let's check, if the input is on the top row:
-if input > top_right:
-	print("input is in top row.")
-	mid = top_right + ring - 1
-	distance_to_mid = input - mid
-	if distance_to_mid < 0:
-		distance_to_mid = -distance_to_mid
-	distance = (ring-1) + distance_to_mid
-	print("manhattan-distance to 1: %d" % distance)
+    if input > top_right:
+            mid = top_right + ring - 1
+            distance_to_mid = input - mid
+            if distance_to_mid < 0:
+                    distance_to_mid = -distance_to_mid
+            distance = (ring-1) + distance_to_mid
+            print("manhattan-distance to 1: %d" % distance)
+            return
 
 # nothing else is the case:
 # input is on the right column
-print("input is in right column.")
-mid = top_right - (ring - 1)
-distance_to_mid = input - mid
-if distance_to_mid < 0:
-	distance_to_mid = -distance_to_mid
-distance = (ring-1) + distance_to_mid
-print("manhattan-distance to 1: %d" % distance)
+    mid = top_right - (ring - 1)
+    distance_to_mid = input - mid
+    if distance_to_mid < 0:
+            distance_to_mid = -distance_to_mid
+    distance = (ring-1) + distance_to_mid
+    print("manhattan-distance to 1: %d" % distance)
+    return
 
 # part 2:
 # As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
@@ -106,14 +99,13 @@ print("manhattan-distance to 1: %d" % distance)
 def ringoffield(field):
 	result = 0
 	square = 0
-	while (square < input): 
+	while (square < field): 
 		result = result + 1
 		square = bottomright(result)
 	return result
 
 def bottomright(ring):
 	result = (2*ring-1)*(2*ring-1)
-	print("bottomright(%d): %d" % (ring, result))
 	return result
 
 def bottomleft(ring):
@@ -151,38 +143,93 @@ def isinleftcol(field):
 
 def left(field):
 	ring = ringoffield(field)
-	print("left(%d)" % field)
-	if isintoprow(field) or (field == topright(ring)):
-		print("toprow")
+	if isinleftcol(field) or (field == topleft(ring)) or (field == bottomleft(ring)):
+		return field + (topleft(ring+1)-topleft(ring)) +1
+	elif isintoprow(field) or (field == topright(ring)):
 		return field+1
 	elif isinbottomrow(field) or (field == bottomright(ring)):
-		print("bottomrow")
 		return field-1
-	elif isinleftcol(field) or (field == topleft(ring)) or (field == bottomleft(ring)):
-		print("leftcol")
-		return field + (topleft(ring+1)-topleft(ring)) +1
+        elif (field == bottomright(ring-1) +1):
+                return field-1
 	elif isinrightcol(field):
-		print("rightcol")
-		print("topright(ring): %d" % topright(ring))
-		print("topright(ring-1): %d" % topright(ring-1))
 		return field - (topright(ring)-topright(ring-1) -1)
+        # should never come here...
 	return "There was an error in left(field)!"
 
 def right(field):
-	return 0
+	ring = ringoffield(field)
+	if isinbottomrow(field) or (field == bottomright(ring)) or (field == bottomleft(ring)):
+		return field+1
+	elif isinrightcol(field) or (field == topright(ring)):
+		return field + (topright(ring+1)-topright(ring) -1)
+	elif isinleftcol(field):
+		return field - (topleft(ring)-topleft(ring-1)+1) 
+	elif isintoprow(field) or (field == topleft(ring)):
+		return field-1
+        # should never come here...
+	return "There was an error in right(field)!"
 
 def top(field):
-	return 0
+	ring = ringoffield(field)
+	if isintoprow(field) or (field == topleft(ring)) or (field == topright(ring)):
+		return field + (topright(ring+1) - topright(ring) + 1)
+	elif isinrightcol(field):
+		return field + 1
+	elif isinleftcol(field) or (field == bottomleft(ring)):
+		return field - 1
+	elif isinbottomrow(field) or (field == bottomright(ring)):
+		return field - (bottomright(ring) - bottomright(ring-1) -1)
+        # should never come here...
+	return "There was an error in top(field)!"
 
 def bottom(field):
-	return 0
+	ring = ringoffield(field)
+	if isinbottomrow(field) or (field == bottomright(ring)) or (field == bottomleft(ring)):
+		return field + (bottomright(ring+1) - bottomright(ring) -1)
+	elif isintoprow(field):
+		return field - (topright(ring) - topright(ring-1) + 1)
+        elif (field == bottomright(ring-1)+1) :
+                return bottomright(ring)
+	elif isinrightcol(field) or (field == topright(ring)):
+		return field - 1
+	elif isinleftcol(field) or (field == topleft(ring)):
+		return field + 1
+        # should never come here...
+	return "There was an error in top(field)!"
 
-value_written = 1
+def part2(input):
+    value_written = 1
 
-#list of values
-values = [1]
-ring = 1
-while value_written <= input:
-	value_written = value_written +1
-print("value_written: %d" % value_written)
-print("left(11): %d" % left(11))
+    #list of values
+    values = [0, 1]
+    field = 1
+    value_written = 1
+    while value_written <= input:
+        field = field + 1
+        value_written = 0
+        if left(field) < field:
+            value_written = value_written + values[left(field)]
+        if left(top(field)) < field:
+            value_written = value_written + values[left(top(field))]
+        if top(field) < field:
+            value_written = value_written + values[top(field)]
+        if top(right(field)) < field:
+            value_written = value_written + values[top(right(field))]
+        if right(field) < field:
+            value_written = value_written + values[right(field)]
+        if bottom(right(field)) < field:
+            value_written = value_written + values[bottom(right(field))]
+        if bottom(field) < field:
+            value_written = value_written + values[bottom(field)]
+        if bottom(left(field)) < field:
+            value_written = value_written + values[bottom(left(field))]
+        values.append(value_written)
+
+    print("value_written: %d" % value_written)
+    
+def main():
+    part1(day_input)
+    part2(day_input)
+
+if __name__ == "__main__":
+    main()
